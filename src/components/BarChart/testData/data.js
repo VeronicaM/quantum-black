31c495d174da543b5719ba2e920c0d40eef4e8c7
Veronica Mihai <mihaiveronica@ymail.com>
2020-04-17 21:38:46 +0300

    add test data for BarChart component tests

---
+++ src/components/BarChart/testData/data.js
@@ -0,0 +1,22 @@
+export const chartValues = [{
+    name: 'banana',
+    value: 20
+}, {
+    name: 'apple',
+    value: 110
+}, {
+    name: 'pear',
+    value: 90
+}, {
+    name: 'orange',
+    value: 50
+}, {
+    name: 'lemon',
+    value: 200
+}, {
+    name: 'pineapple',
+    value: 150
+}];
+
+export const testExistingItem = { name: "banana", value: 10 };
+export const testNewItem = { name: "cherry", value: 100 } 
